1ec8778fa5e47c582726468c194ddca409566feb
Avikam Chauhan <avikamchauhan@icloud.com>
2020-05-28 15:08:53 -0700

    Added LocationHandler and MapHandler
    Still work-in-progress: inter-class integration and protocols

---
+++ PacMan Coronatrainer/LocationHandler.swift
@@ -0,0 +1,60 @@
+//
+//  MapHandler.swift
+//  PacMan Coronatrainer
+//
+//  Created by Avikam on 5/27/20.
+//  Copyright © 2020 Avikam Chauhan. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    internal var locationManager: CLLocationManager?
+    static var lastUpdatedLocation: CLLocation?
+    static var lastUpdatedHeading: CLHeading?
+    
+    var delegate: LocationHandlerDelegate?
+    
+    override init() {
+        super.init()
+        
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func startLocationUpdates() {
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingLocation();
+        locationManager?.startUpdatingHeading();
+    }
+    
+    internal func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        LocationHandler.lastUpdatedHeading = newHeading
+        delegate?.didUpdateCurrentHeading(currentHeading: newHeading)
+    }
+    
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        LocationHandler.lastUpdatedLocation = locations.first
+        delegate?.didUpdateCurrentLocation(currentLocation: locations.first)
+    }
+    
+    static func getCurrentLocation() -> CLLocation? {
+        return self.lastUpdatedLocation
+    }
+    
+    static func getCurrentHeading() -> CLHeading? {
+        return self.lastUpdatedHeading
+    }
+    
+}
+
+protocol LocationHandlerDelegate {
+    
+    func didUpdateCurrentLocation(currentLocation: CLLocation?)
+    
+    func didUpdateCurrentHeading(currentHeading: CLHeading)
+    
+}
+
